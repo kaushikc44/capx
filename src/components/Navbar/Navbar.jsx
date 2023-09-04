a96c468369fb6758c6cc18f837a67d11aa7b0ac9
kaushikchoudhury <kaushikc44@gmail.com>
2023-09-04 23:14:56 +0530

    fixed the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,19 @@
-export default function Navbar(){
-    return(
-        <>
-        <div className="navbar bg-base-100 flex flex-row ">
-        <ul className="flex flex-row justify-item-center items-center">
+export default function Navbar() {
+    return (
+      <>
+        <div className="navbar bg-base-100 flex flex-row h-[157px] w-[1261px] justify-items-center bg-capx-black">
+          <ul className="flex flex-row justify-item-center items-center">
             <li className="mr-10 hover:underline decoration-capx-green decoration-3">Bounties</li>
             <li className="mr-10 hover:underline decoration-capx-green decoration-3">Quest</li>
             <li className="mr-10 hover:underline decoration-capx-green decoration-3">Opportunities</li>
             <li className="mr-10 hover:underline decoration-capx-green decoration-3">Reach Out</li>
-        </ul>
-        <div className="navbar-end">
+          </ul>
+          <div className="navbar-end">
             <button className="btn btn-ghost btn-circle">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
             </button>
+          </div>
         </div>
-        </div>
-
-        </>
+      </>
     )
-}
+  }
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,8 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors:{
-        'capx-green':'#8dbc1a'
+        'capx-green':'#8dbc1a',
+        'capx-black' : '#0e0e0e',
       },
     },
   },
