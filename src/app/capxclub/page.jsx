96d918f2ff70ce212a372116ca5cd7010eaa769b
kaushikchoudhury <kaushikc44@gmail.com>
2023-09-04 18:48:34 +0530

    navbar

---
+++ src/app/capxclub/page.jsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar/Navbar"
+export default function CapxClub(){
+    return(
+        <>
+            <div>
+                <div>
+                    <Navbar />
+                </div>
+            </div>
+        </>
+    )
+}
