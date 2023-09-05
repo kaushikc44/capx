24e0c9989e8957da916365890855bcd08d1fcf7a
Madhan-404 <madhansainik@gmail.com>
2023-09-06 02:48:07 +0530

    background fix

--- src/app/capxclub/page.jsx
+++ src/app/capxclub/page.jsx
@@ -1,11 +1,14 @@
-import Navbar from "@/components/Navbar/Navbar"
+// import Navbar from "@/components/Navbar/Navbar"
 import SideBar from "@/components/sidebar/Sidebar"
+import Background from "@/components/Background/Background"
+
 export default function CapxClub(){
     return(
         <>
             <div>
                 <div>
                     {/* <Navbar /> */}
+                    <Background />
                     <SideBar />
                 </div>
             </div>
---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Background = () => {
+  return (
+    <div className="background-container">
+      <div className="ellipse-16a"></div>
+      <div className="ellipse-17a"></div>
+      <div className="ellipse-18a"></div>
+      <div className="ellipse-19a"></div>
+      <div className="ellipse-20a"></div>
+      <div className="ellipse-21a"></div>
+      <div className="ellipse-16b"></div>
+      <div className="ellipse-17b"></div>
+      <div className="ellipse-18b"></div>
+      <div className="ellipse-19b"></div>
+      {/* <div className="vector"></div> */}
+    </div>
+  )
+}
+
+export default Background
