35eacb32764d50f2cfbdf849e6522ee1250796b9
kaushikchoudhury <kaushikc44@gmail.com>
2023-09-10 20:16:50 +0530

    added the cards to the main page

--- src/app/capxclub/page.jsx
+++ src/app/capxclub/page.jsx
@@ -6,22 +6,50 @@ import Card from "@/components/card/Card"
 import Navbar from "@/components/Navbar/Navbar"
 
 export default function CapxClub(){
+    
+    const cardData = [
+        { title: "Card 1", description: "Description 1", /* other properties */ },
+        { title: "Card 2", description: "Description 2", /* other properties */ },
+        { title: "Card 1", description: "Description 1", /* other properties */ },
+        { title: "Card 2", description: "Description 2", /* other properties */ },
+        { title: "Card 1", description: "Description 1", /* other properties */ },
+        { title: "Card 2", description: "Description 2", /* other properties */ },
+        // Add more card data objects here
+      ];
+
+    
+        
+
     return(<>
     
     
-        <div className="relative">
+    <div className="relative">
         {/* Background Wallpaper with z-index: -1 */}
-        <div className="absolute inset-0 z-[-1]">
-            <BackgroundWallpaper />
+        <div className="absolute inset-0 z-0">
+          <BackgroundWallpaper />
         </div>
-        <div className="flex flex-row  z-[1]">
-            <Navbar className=""/>
-            <SideBar/>
-            
+        <div className="flex flex-row relative z-10">
+          <div className="flex flex-row">
+            <Navbar className="" />
+            <SideBar />
+          </div>
+          <div className="flex-grow ml-[180px] mt-[160px]">
+            <div className="flex flex-wrap">
+              {cardData.map((card, index) => (
+                <div key={index} className="w-1/4 p-4">
+                  <Card
+                    title={card.title}
+                    description={card.description}
+                    /* other properties */
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-        {/* Your Card component */}
-        {/* <Card title={"Polygon"} description={"Polygon Chain"} chain={"polygon"} currency={"usdc"} amount={"1700"} EndDate={"7 Days"} /> */}
-    </div>
+      </div>
+       
+    
     </>
     )
 }
